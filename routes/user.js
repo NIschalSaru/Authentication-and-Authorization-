7a18bfee910037d56e3363b5bfd728aed9916903
NIschalSaru <mnischal29@gmail.com>
2024-07-26 11:56:54 +0545

    commit

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
-router.post("/", async (req, res) => {
+router.post("/signUp", async (req, res) => {
   try {
     const data = req.body;
     const newUser = new User(data);
